a5672bc794ed7fba69fe135edc3e0ffd18e80f15
Kunal Dharpure <146354402+kunald08@users.noreply.github.com>
2025-03-10 23:19:45 +0530

    Initial commit

---
+++ prog.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int arr[];
+    int num, num2, i;
+    string str;
+    cin >> num >> num2 >> str;
+    if (num == num2)
+    {
+        cout << "Numbers_are_equal";
+    }
+    for (i = 0; i < num; i++)
+    {
+        cout << str;
+    }
+    return 0;
+}
